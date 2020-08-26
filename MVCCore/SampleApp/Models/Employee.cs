52c46446eb979754801d0909f6ea923bd0dc7d6c
Luis Capuyon <frostehhh967@gmail.com>
2020-08-26 17:49:39 +0800

    Added MVC/Core approach

---
+++ MVCCore/SampleApp/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApp.Models
+{
+    public class Employee
+    {
+        [Required]
+        public string Owner { get; set; }
+        [Required(ErrorMessage = "Status is required")]
+        public int Status { get; set; }
+    }
+}
