6d4479e3849e5ef1c1c46ec843569a3827c27536
LexDX <42230789+LexDX@users.noreply.github.com>
2021-02-12 10:42:39 +0300

    fix validator

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { DxDropDownBoxModule } from 'devextreme-angular/ui/drop-down-box';
 import { DxFormModule } from 'devextreme-angular/ui/form';
 import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
-import { DxValidatorModule } from 'devextreme-angular/core';
+import { DxValidatorModule } from 'devextreme-angular/ui/validator';
 import { AppComponent } from './app.component';
 
 @NgModule({
